88dcf916be9763725280cc72ffa517d908de776e
dayCod <alayarahman07@gmail.com>
2023-07-11 11:19:12 +0700

    Update: Find by id & insert data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +16,37 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
+| Find by Id
+|--------------------------------------------------------------------------
+*/
+Route::get('find', function () {
+    return json_encode(DB::select("SELECT * FROM users WHERE id = ?", [1]));
+});
+
+
+/*
+|--------------------------------------------------------------------------
 | Read
 |--------------------------------------------------------------------------
 */
 Route::get('/read', function () {
     return json_encode(DB::select("SELECT * FROM users ORDER BY id ASC"));
 });
+
+/*
+|--------------------------------------------------------------------------
+| Create / Insert
+|--------------------------------------------------------------------------
+*/
+Route::get('/create', function () {
+    return (DB::insert("INSERT INTO users(name, email, email_verified_at, password, remember_token) VALUEs(?, ?, ?, ?, ?) ", [
+        'John Doe',
+        'johndoe@mail.com',
+        Carbon::now(),
+        bcrypt('password'),
+        Str::random(8),
+    ]));
+});
+
 
 
